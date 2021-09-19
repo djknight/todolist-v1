c53bd6c4de716b0df7728138f66cd99071044532
Djknight <dan90kn@gmail.com>
2021-09-19 17:12:44 +1000

    forloop added fixed LI and button

--- app.js
+++ app.js
@@ -3,29 +3,34 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+//items to render html item array
+var items = [];
+
 //ejs to use the view engine
 app.set("view engine", "ejs");
 //bodyParser for newItem
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", function (req, res) {
-
   var today = new Date();
 
-var options = {
-    weekday:"long",
-    day:"numeric",
-    month:"long"
-};
-  var day = today.toLocaleDateString("en-US",options);
+  var options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long",
+  };
+  var day = today.toLocaleDateString("en-US", options);
 
   //ejs file render html
-  res.render("list", { kindOfDay: day });
+  res.render("list", { kindOfDay: day, newListItem: items });
 });
-//post newItem function
-app.post("/",function(req, res){
-    var item = req.body.newItem;
-    console.log(item);
+//post newItem function item
+app.post("/", function (req, res) {
+  var item = req.body.newItem;
+  //array use
+  items.push(item);
+  //redirect for item home
+  res.redirect("/");
 });
 
 app.listen(3000, function () {
