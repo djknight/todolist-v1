6c58474178da71fb08a3a20c541ed5e356567cbc
Djknight <dan90kn@gmail.com>
2021-09-21 10:45:54 +1000

    date  now is its own date.js

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
 
 const app = express();
 
@@ -14,20 +15,11 @@ app.set("view engine", "ejs");
 
 //bodyParser for newItem
 app.use(bodyParser.urlencoded({ extended: true }));
-
-//css
 app.use(express.static("public"));
+
 app.get("/", function (req, res) {
-  let today = new Date();
-
-  let options = {
-    weekday: "long",
-    day: "numeric",
-    month: "long",
-  };
-
-  let day = today.toLocaleDateString("en-US", options);
-
+  //calling date
+  let day = date();
   //ejs file render html
   res.render("list", { listTitle: day, newListItem: items });
 });
---
+++ date.js
@@ -0,0 +1,16 @@
+//modules exports.getDate  <--- exports.function
+module.exports.getDate = getDate;
+
+function getDate() {
+  let today = new Date();
+
+  let options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long",
+  };
+
+  let day = today.toLocaleDateString("en-US", options);
+
+  return day;
+}
